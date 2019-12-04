c50fa5ec206452dd0d5bbbbe3079760528cbc1d1
Daniel Zhao <danielantoiny@gmail.com>
2019-12-03 23:23:52 -0500

    add spanish

---
+++ 01_translator/README.md
@@ -0,0 +1,15 @@
+# Translator
+
+## Example Command
+
+```shell
+./spanish -f table
+> mesa
+
+./japanese -f I love you
+> あなたを愛してる
+```
+
+## Supported Target Langauges
+
+Chinese, Japanese, English, Spanish and Arabic
---
+++ 01_translator/spanish.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	// "flag"
+	"net/http"
+	"net/url"
+	"io/ioutil"
+	"encoding/json"
+)
+
+const gt = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=%s&dt=t&q=%s"
+
+
+func main() {
+	// sentencePtr := flag.String("for", "", "Provide a word or a sentence to translate")
+
+	// flag.Parse()
+
+	argsWithoutProg := os.Args[2:]
+	sentence := strings.Join(argsWithoutProg," ")
+
+	url := fmt.Sprintf(gt, "es", url.PathEscape(sentence))
+
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+    responseData, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+		panic(err)
+	}
+
+    var resp []interface{}
+	
+	err = json.Unmarshal(responseData, &resp)
+	if err != nil {
+		panic(err)
+	}
+
+	translatedText := ""
+	for _, obj := range resp[0].([]interface{}) {
+
+		if len(obj.([]interface{})) == 0 {
+			break
+		}
+		translatedText += obj.([]interface{})[0].(string)
+	}
+
+	fmt.Println(translatedText)
+}
