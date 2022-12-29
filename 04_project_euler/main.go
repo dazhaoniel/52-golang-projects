783d56e2dc3b3cf5f017f9be840d8f9fe5481848
Daniel Zhao <danielantoiny@gmail.com>
2022-12-29 02:12:57 -0600

    add find prime

--- 04_project_euler/main.go
+++ 04_project_euler/main.go
@@ -30,6 +30,34 @@ func sumSquareDifference() float64 {
 	return math.Pow(squareOfSum, 2) - sumOfSquare
 }
 
+func isPrime(n int) bool {
+	if n == 1 {
+		return false
+	}
+	for i := 2; i < int(math.Sqrt(float64(n)))+1; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// https://projecteuler.net/problem=7
+func tenThoughsandthPrime() int {
+	c := 1
+	n := 3
+	for n > 0 {
+		if isPrime(n) {
+			c += 1
+		}
+		if c == 10001 {
+			break
+		}
+		n += 1
+	}
+	return n
+}
+
 func main() {
-	fmt.Println(sumSquareDifference())
+	fmt.Println(tenThoughsandthPrime())
 }
